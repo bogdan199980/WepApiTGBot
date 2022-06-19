d9ba80ebb4b79cc673fe8f709756b7d7f5d68900
Bogdan <Bogdan@HOME-PC>
2022-06-20 00:18:06 +0300

    Тест

---
+++ WepApiTGBot/Controllers/BotController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace WepApiTGBot.Controllers
+{
+    [Route("api/bot")]
+    [ApiController]
+    public class BotController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Update update)
+        {
+
+
+            TelegramBotClient client = new TelegramBotClient("1745794348:AAE0I_rNE6iDCOAWhqGK17icxJs_zkvBp_Y");
+            if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+            {
+                await client.SendTextMessageAsync(update.Message.From.Id, "answer");
+            }
+            return Ok();
+
+        }
+    }
+}
